861a20fcb38804197023bc6f235b0873aa648c41
Vangeanc3 <ismaelmartins919@gmail.com>
2022-12-26 20:26:01 -0300

    add branchs

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -7,6 +7,11 @@ namespace APP_API.Models
 {
     public class Usuario
     {
-        
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Telefone { get; set; }
+        public string? Email { get; set; }
+        public string? Endereco { get; set; }
+        public string? Cpf { get; set; } 
     }
 }
