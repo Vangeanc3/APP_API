dcd3c6ccd0cabcde1b520d27844514c8c85abe64
est_imartins <ismaelmartins919@gmail.com>
2023-01-26 15:10:03 -0300

    add git ignore

---
+++ Models/Vendedor.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace APP_API.Models
+{
+    public class Vendedor : Usuario
+    {
+        
+        public int Id { get; set; }
+        
+    }
+}
